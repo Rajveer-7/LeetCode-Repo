af29e9421d9b5012b507fae3a695b4fe6fa5f5a8
Rajveer Singh <118693440+Rajveer-7@users.noreply.github.com>
2025-04-23 04:17:30 +0000

    Create file

---
+++ 1-TwoSum/1-TwoSum.java
@@ -0,0 +1,26 @@
+// Last updated: 4/23/2025, 12:17:28 AM
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        // We create a hashmap to store numbers and their indices
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // iterate through array 
+        for (int i = 0; i < nums.length; i++) {
+
+            int complement = target - nums[i];
+
+            // Check if complement is in the map 
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i};
+                
+            }
+            // add current number and index to array
+            map.put(nums[i], i);
+        }
+    // Return empty array if no solution is found
+        return new int[] {};
+
+    }
+}
