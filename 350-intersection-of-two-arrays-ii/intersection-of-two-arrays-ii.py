78da9a76fc4378a81edd64d4a1afb9fd837d75d1
Rajveer Singh <118693440+Rajveer-7@users.noreply.github.com>
2025-11-24 11:20:47 -0500

    Time: 1 ms (52.66%) | Memory: 17.8 MB (73.89%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        ## two pointer solution 
+        nums1, nums2 = sorted(nums1), sorted(nums2)
+        p1 = p2 = 0
+        n, m = len(nums1), len(nums2)
+        ans = []
+
+        while p1 < n and p2<m:
+            if nums1[p1] == nums2[p2]:
+                ans.append(nums1[p1])
+                p1+=1
+                p2+=1
+                continue
+            
+            if nums1[p1] > nums2[p2]:
+                p2 += 1
+            else:
+                p1 += 1
+
+        return ans
